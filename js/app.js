454acb22fbbb0beb151758876687d24abe336cee
Matthew T. Renwick <mrenny65@gmail.com>
2022-07-17 22:00:28 -0600

    attempted problem Three

--- js/app.js
+++ js/app.js
@@ -8,15 +8,15 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-//   let mySum = a + b;
-//   let myStr = `The sum of ${a} and ${b} is ${mySum}.`;
+function sum(a, b) { //eslint-disable-line
+  let mySum = a + b;
+  let myStr = `The sum of ${a} and ${b} is ${mySum}.`;
 
-//   return [mySum, myStr];
-// }
+  return [mySum, myStr];
+}
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,11 +54,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sum1 = sum(`${mySum} +`
+  let str1 = `${a} and ${b} and ${c} sum to 16`;
+  let str2 = `The product of ${a} and ${b} and ${c} is 140`;
 
-}
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+  
+  // Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -132,3 +135,4 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+}
